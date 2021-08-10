69781f38606bc59e82c3f62a52bb6bb159fdb56a
Mikhail Klishevich <x-ray@mail.ru>
2021-08-11 04:05:14 +0700

    Initial Commit

---
+++ PosApiTest/PosApiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PosApiTestApp.swift
+//  PosApiTest
+//
+//  Created by Mikhail Klishevich on 11.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct PosApiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
